4dd80abb1e65a3282ceeeb191ce4e0cf74ca2637
Sparsh Modi <sparshmodi2016@gmail.com>
2025-02-05 18:05:56 -0500

    removed default nextjs code

---
+++ src/pages/_app.tsx
@@ -0,0 +1,19 @@
+import { AppProps } from 'next/app'
+import Head from 'next/head'
+import RootLayout from './layout';
+
+export default function App({
+	Component,
+	pageProps,
+}: AppProps): React.ReactNode {
+	return (
+		<>
+			<Head>
+				<title>WatMovie</title>
+			</Head>
+      <RootLayout>
+        <Component {...pageProps} />
+      </RootLayout>
+    </>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+
+export default function Home() {
+    return (
+        <div>
+        <main>
+            <h1>Welcome to WatMovie</h1>
+            <p>Find your next favorite movie!</p>
+        </main>
+        </div>
+    );
+    }
