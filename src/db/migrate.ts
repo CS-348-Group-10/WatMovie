84459c3960381b61c4196ed154f4f457af44cb49
Sparsh Modi <sparshmodi2016@gmail.com>
2025-02-07 15:35:16 -0500

    fix eslinttttt

--- src/db/migrate.ts
+++ src/db/migrate.ts
@@ -1,28 +1,28 @@
-import pool from './index'
 import fs from 'fs'
 import path from 'path'
+import pool from './index'
 
 const runMigrations = async () => {
-  const client = await pool.connect()
-  try {
-    console.log("🚀 Running Migrations...")
+	const client = await pool.connect()
+	try {
+		console.log('🚀 Running Migrations...')
 
-    const migrationDir = path.join(process.cwd(), "src/db/migrations")
-    const migrationFiles = fs.readdirSync(migrationDir).sort()
+		const migrationDir = path.join(process.cwd(), 'src/db/migrations')
+		const migrationFiles = fs.readdirSync(migrationDir).sort()
 
-    for (const file of migrationFiles) {
-      const filePath = path.join(migrationDir, file)
-      const sql = fs.readFileSync(filePath, "utf-8")
-      console.log(`📄 Running migration: ${file}`)
-      await client.query(sql)
-    }
+		for (const file of migrationFiles) {
+			const filePath = path.join(migrationDir, file)
+			const sql = fs.readFileSync(filePath, 'utf-8')
+			console.log(`📄 Running migration: ${file}`)
+			await client.query(sql)
+		}
 
-    console.log("✅ Migrations completed!")
-  } catch (error) {
-    console.error("❌ Migration failed:", error)
-  } finally {
-    client.release()
-  }
-};
+		console.log('✅ Migrations completed!')
+	} catch (error) {
+		console.error('❌ Migration failed:', error)
+	} finally {
+		client.release()
+	}
+}
 
 export default runMigrations
