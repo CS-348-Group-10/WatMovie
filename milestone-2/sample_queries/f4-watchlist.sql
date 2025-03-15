1d76a0a3f4fe3f2df5696744e992710d5d086d5d
Udit Khattar <uditkhattar@gmail.com>
2025-03-14 22:09:40 -0400

    fixed syntax in sql query

--- milestone-2/sample_queries/f4-watchlist.sql
+++ milestone-2/sample_queries/f4-watchlist.sql
@@ -1,4 +1,4 @@
 SELECT * 
 FROM watchlists
 LEFT JOIN titles ON watchlists.title_id = titles.title_id
-WHERE watchlists.user_id = 8
+WHERE watchlists.user_id = 8;
--- milestone-2/sample_queries/f5-user-review.sql
+++ milestone-2/sample_queries/f5-user-review.sql
@@ -1,4 +1,4 @@
 SELECT name, review, rating
 FROM user_review as t1 
 NATURAL JOIN users t2
-WHERE title_id = 'tt17528314'
+WHERE title_id = 'tt17528314';
