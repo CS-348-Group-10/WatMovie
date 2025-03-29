44572a458bf5a892de90c1cbeaf462eeeb452212
Udit Khattar <uditkhattar@gmail.com>
2025-03-29 04:08:49 -0400

    revert user review query to original

--- src/db/queries/userReviews/insertUserReview.ts
+++ src/db/queries/userReviews/insertUserReview.ts
@@ -1,21 +1,6 @@
-// Base query without timestamps
-export const insertUserReviewQuery = `
-INSERT INTO user_reviews (
-  uid, mid, rating, comment
-) 
-VALUES (
-  $1, $2, $3, $4
-)
-RETURNING *;
+export const insertUserReviewQuery =
 `
-
-// Query with timestamps
-export const insertUserReviewWithTimestampsQuery = `
-INSERT INTO user_reviews (
-  uid, mid, rating, comment, created_at, updated_at
-) 
-VALUES (
-  $1, $2, $3, $4, $5, $6
-)
+INSERT INTO user_reviews (uid, mid, rating, comment) 
+VALUES ($1,$2,$3,$4)
 RETURNING *;
 `
