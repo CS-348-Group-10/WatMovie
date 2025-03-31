4d02a23f2d119d407685578d946130254f3c8bb6
Medhansh Hinduja <mhinduja@uwaterloo.ca>
2025-03-31 00:38:51 -0400

    M3 testing (#66)
    * added sample query + outputs
    
    * added prod queries + outputs

---
+++ milestone-3/production_queries/f4-select-review.sql
@@ -0,0 +1,12 @@
+SELECT
+    uid, 
+    first_name,
+    last_name,
+    rating,
+    comment,
+    created_at,
+    updated_at
+FROM user_reviews
+NATURAL JOIN users
+WHERE mid = 'tt2990738'
+LIMIT 10;
---
+++ milestone-3/sample_queries/f2-select-watchlist-movies.sql
@@ -0,0 +1,22 @@
+SELECT 
+    M.mid AS id,
+    M.name AS movie,
+    M.is_adult,
+    M.release_year,
+    M.runtime_minutes AS duration,
+    MR.sum_of_votes / MR.total_votes AS rating,
+    MR.total_votes AS votes,
+    CASE
+        WHEN COUNT(GM.gid) = 0 THEN NULL
+        ELSE ARRAY_AGG(GM.gid)
+    END AS genre_ids
+FROM movies M
+INNER JOIN watchlists W ON M.mid = W.mid
+LEFT JOIN imdb_ratings MR ON M.mid = MR.mid
+LEFT JOIN genres_movies GM ON M.mid = GM.mid
+WHERE 
+    W.uid = 1
+GROUP BY 
+    M.mid, 
+    MR.sum_of_votes, 
+    MR.total_votes;
